b1ab93f7fff5a928044a898ef16fd1080bf10ff5
DouglasWWolf <douglaswolf@yahoo.com>
2024-04-24 21:24:40 -0700

    No commit message

--- example/example.sh
+++ example/example.sh
@@ -36,7 +36,7 @@ test $(is_bitstream_loaded) -eq 0 && need_bitstream=1
 # If we need to load the bitstream into the FPGA, make it so
 if [ $need_bitstream -eq 1 ]; then
     echo "Loading bitstream..."
-    source load_bc_emu
+    source load_bitstream.sh 
     test $? -eq 0 || exit 1
     echo "Bitstream loaded"
 fi
