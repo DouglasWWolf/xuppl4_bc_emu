dc0b45e7ce118e18b7f626c87c6256b6ffc0a39b
DouglasWWolf <douglaswolf@yahoo.com>
2024-06-21 16:40:57 -0700

    No commit message

--- example/load_bitstream.sh
+++ example/load_bitstream.sh
@@ -1,1 +1,1 @@
-load_bitstream -hot_reset -config load_jtag.conf -part xcvu3p_0 xuppl4_bc_emu.bit
+load_bitstream -hot_reset -config load_jtag.conf -part xcvu3p_0 -pci_id 10ee:903f -vivado $VIVADO xuppl4_bc_emu.bit
